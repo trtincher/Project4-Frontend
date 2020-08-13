a34192255bbee5b1c8bc9e3b94a1c8c9a27e476f
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-08-13 17:51:49 -0500

    added decks, hands, ActionHand, SpellsHand files

--- familiar-app-client/src/Components/Dashboard/DashBody/Hands/ActionHand.jsx
+++ familiar-app-client/src/Components/Dashboard/DashBody/Hands/ActionHand.jsx
@@ -1,19 +1,14 @@
 import React, { useState, useContext } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import apiURL from '../../../apiConfig';
+import apiURL from '../../../../apiConfig';
 import axios from 'axios';
 import styled, { css } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGavel, faBookmark, faBook, faTools } from '@fortawesome/free-solid-svg-icons';
-import { DataContext } from '../../../App';
+import { DataContext } from '../../../../App';
 
-import Decks from './Decks/Decks';
-import Hands from './Hands/Hands';
-
-const Container = styled.div`
-	margin: 0;
-	height: 1000px;
-`;
+import CardContainer from '../../../Card/CardContainer';
+import Card from '../../../Card/Card';
 
 const actionColor = '#FEE440';
 const spellColor = '#9b5de5';
@@ -26,15 +21,10 @@ const bookmark = <FontAwesomeIcon icon={faBookmark} size="6x" />;
 const book = <FontAwesomeIcon icon={faBook} size="6x" />;
 const tools = <FontAwesomeIcon icon={faTools} size="6x" />;
 
-function DashBody() {
+function ActionHand() {
 	const { activeCharacter, setActiveCharacter } = useContext(DataContext);
 
-	return (
-		<Container>
-			<Decks />
-			<Hands />
-		</Container>
-	);
+	return <CardContainer />;
 }
 
-export default DashBody;
+export default ActionHand;
