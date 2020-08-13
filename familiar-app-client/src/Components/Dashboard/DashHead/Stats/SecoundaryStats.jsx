3a95910b10096524bf33bb012350383197df8436
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-08-13 13:34:40 -0500

    secoundary stats live

--- familiar-app-client/src/Components/Dashboard/DashHead/Stats/SecoundaryStats.jsx
+++ familiar-app-client/src/Components/Dashboard/DashHead/Stats/SecoundaryStats.jsx
@@ -381,6 +381,31 @@ function SecoundaryStats({ character }) {
 
 	let init = modifiers.dexterity;
 
+	const profTable = {
+		1: 2,
+		2: 2,
+		3: 2,
+		4: 2,
+		5: 3,
+		6: 3,
+		7: 3,
+		8: 3,
+		9: 4,
+		10: 4,
+		11: 4,
+		12: 4,
+		13: 5,
+		14: 5,
+		15: 5,
+		16: 5,
+		17: 6,
+		18: 6,
+		19: 6,
+		20: 6
+	};
+
+	let prof = profTable[currentCharacter.level];
+
 	return (
 		<StatsContainer>
 			<Stat>
@@ -397,7 +422,7 @@ function SecoundaryStats({ character }) {
 			</Stat>
 			<Stat>
 				<StatName>Prof</StatName>
-				<Modifier>+2</Modifier>
+				<Modifier>{prof}</Modifier>
 			</Stat>
 		</StatsContainer>
 	);
