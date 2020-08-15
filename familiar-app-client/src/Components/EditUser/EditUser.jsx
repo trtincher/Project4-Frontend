caac5da0ff8806b2fa49fe1e4c0581f2e8edcbea
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-08-15 12:54:48 -0500

    EditUser edits, Delete works!

--- familiar-app-client/src/Components/EditUser/EditUser.jsx
+++ familiar-app-client/src/Components/EditUser/EditUser.jsx
@@ -56,12 +56,12 @@ function EditUser(props) {
 				url: `${apiURL}/users/${id}`,
 				method: 'DELETE'
 			});
-
-			console.log('Delect res', res);
-			setIsDeleted(true);
 		} catch (err) {
 			console.error(err);
 		}
+
+		console.log('Delect res', res);
+		setIsDeleted(true);
 	};
 
 	const deleteCard = (
